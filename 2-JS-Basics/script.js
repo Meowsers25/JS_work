3d11323921fbb9dadc3c8c8f9aa4f8794788537d
Chris <c.lamothe25@gmail.com>
2018-04-02 22:46:45 -0400

    data types and variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+var name = "Chris";
+var lastName = "Sanchez";
+var age = 38;
+var fullAge = true;
+
+console.log(name);
+console.log(lastName);
+console.log(age);
+console.log(fullAge);
+
+document.write("<p>"+name+" "+lastName+" "+age+"</p>");
